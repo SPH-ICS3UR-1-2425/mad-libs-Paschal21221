08298455f799bcd407082c3f49b83c139024ffb3
Paschal21221 <paschal.nzewuji@stu.ocsb.ca>
2024-09-24 14:09:30 -0400

    Madlibs assignment
    im finnal done!!!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Main {
-
+	
 	public static void main(String[] args) {
 		/*
 		 * MadLibs Program
@@ -12,10 +12,8 @@ public class Main {
 		 * Make sure to include Strings, ints, and doubles!
 		 */
 		
-		Scanner in = new Scanner(System.in){
-		System.out.println("Welcome to Mad Libs Whats your name?");
-		String name = in.nextLine();
-		System.out.println("What Year Comes to mind?");
+		Scanner in = new Scanner(System.in);
+		System.out.println("Welcome to Mad Libs!,What Year Comes to mind?");
 		int year = in.nextInt();
 		System.out.println("Give me a country");
 		in.nextLine();
@@ -37,7 +35,8 @@ public class Main {
 		String dance = in.nextLine();
 		System.out.println("Give me an attack");
 		String attack = in.nextLine();
-		System.out.println("The Year is "+year+"  And aliens have landed in "+country+" , The aliens closley resemble "+animal+" . They claim they come from "+planet+" and are here to make friends. "+friendname+" and I offer to be thier friend and they let us in thier spaceship it kind of looks like a "+object+" Once we are inside we teach them how to play "+sport+" and they love it. WE play for "+numberofdays+" days but we then realize they wont let us leave.So when one comes over we do the "+dance+" to distract them and when hen isnt paying attention we "+attack+" Him and escape.  ");
+		System.out.println("The Year is "+year+"  And aliens have landed in "+country+" , The aliens closley resemble "+animal+" . They claim they come from "+planet+" and are here to make friends. "+friendname+" and I offer to be thier friend and they let us in thier spaceship it kind of looks like a "+object+" Once we are inside we teach them how to play "+sport+" and they love it. WE play for "+numberofdays+" days but we then realize they wont let us leave.So when one comes over we do the "+dance+" to distract them and when hen isnt paying attention we "+attack+" Him and escape.  ");	
 	}
-
+		
+}
 
